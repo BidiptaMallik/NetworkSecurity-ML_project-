c1376bdbf380da055890260605462681a4bd3c40
Bidipta Mallik <bidiptamallik0@gmail.com>
2026-06-12 21:58:02 +0530

    Deployment Changes Done

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ ca = certifi.where()
 from dotenv import load_dotenv
 load_dotenv()
 mongo_db_url=os.getenv("MONGODB_URL_KEY")
-print(mongo_db_url)
+
 import pymongo
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
@@ -92,4 +92,4 @@ async def predict_route(request:Request,file:UploadFile=File(...)):
     
 
 if __name__=="__main__":
-    app_run(app,host="localhost",port=8000)
+    app_run(app,host="0.0.0.0",port=8000)
